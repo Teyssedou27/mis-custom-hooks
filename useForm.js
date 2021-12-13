a1af94a695200594441447173119d989993ed4fe
Juan Teyssedou <jp.teyssedou@aconpy.com>
2021-12-13 20:40:24 -0300

    primer commit

---
+++ useForm.js
@@ -0,0 +1,18 @@
+import { useState } from "react";
+
+
+export const useForm = (initialState = {}) => {
+   const [value, setValue] = useState(initialState)
+
+
+const inputHanldeChange = ({target}) => {
+    
+    setValue({
+        ...value,
+        [target.name] : target.value
+    });
+}
+
+
+return [value, inputHanldeChange]
+}
